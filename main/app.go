d13f97e0b7b7ca02eb0ce5d6dd6e54385bb57adb
ipahomov <ipahomov13@gmail.com>
2017-07-21 23:33:28 +0300

    init

---
+++ main/app.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"deseptibot/model"
+)
+
+func main() {
+	fmt.Print("Hello, Vadik!")
+
+	pair := model.CurrencyPair{
+		9,
+		9,
+		9,
+	}
+
+	fmt.Print(pair)
+}
---
+++ model/currencyPair.go
@@ -0,0 +1,7 @@
+package model
+
+type CurrencyPair struct {
+	Last uint
+	High uint
+	Low uint
+}
